043b759bf6381de632fbe7cdc76e290cd7dbab92
jasonheard <heardjasonl@gmail.com>
2015-11-07 16:39:30 -0500

    version 0.1

---
+++ README.md
@@ -0,0 +1,18 @@
+Zippy
+===
+
+Collaborators
+---
+Jason Heard
+
+License
+---
+Do what you will
+
+Project Summary
+---
+My first iOS App using Swift 2. It connects to http://www.zipcodeapi.com and parses the JSON. 
+
+Versions
+---
+- 0.1 -  It return the state, city for the zip code entered
--- Zippy/ViewController.swift
+++ Zippy/ViewController.swift
@@ -10,16 +10,84 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var zippyTitle: UILabel!
+    @IBOutlet weak var zipcodeTextField: UITextField!
+    @IBOutlet weak var zipCodeLabel: UILabel!
+    @IBOutlet weak var zipCodeInfoLabel: UILabel!
+    @IBOutlet weak var stateLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var stateInfoLabel: UILabel!
+    @IBOutlet weak var cityInfoLabel: UILabel!
+    @IBOutlet weak var zipcodeIndicator: UIActivityIndicatorView!
+    
+    let apiKey = "ia6bDSOSCQG8CUMw6E5gSaj1rf057oiqzpCdiq5UgM3TGPpNuv4nGno7CocKNvbm"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func searchForZipCode(sender: AnyObject) {
+        
+        var state = ""
+        var city = ""
+        var zipcode = ""
+        var finished = false
+        
+        callZipcodeApi() { jsonData in
+            if(jsonData != nil) {
+                state = String(jsonData!["state"]!)
+                city = String(jsonData!["city"]!)
+                zipcode = self.zipcodeTextField.text!
+            } else {
+                state = ""
+                city = ""
+                zipcode = "Zipcode not Found!"
+            }
+            finished = true
+        }
+        
+        //TODO: HACK: this doesn't feel like the best way to handle the asynchronous task from what I've read.
+        while(finished == false) {}
+        
+        stateInfoLabel.text = state
+        cityInfoLabel.text = city
+        zipCodeInfoLabel.text = zipcode
+        zipcodeTextField.text = ""
+    }
+    
+    func callZipcodeApi(completionHandler: (NSDictionary?) -> Void ) -> NSURLSessionTask {
 
+        
+        let jsonUrl = "https://www.zipcodeapi.com/rest/\(apiKey)/info.json/\(self.zipcodeTextField.text!)/degrees"
+        
+        let session = NSURLSession.sharedSession()
+        let zipcodeApiUrl = NSURL(string: jsonUrl)
+        
+        let task : NSURLSessionDataTask = session.dataTaskWithURL(zipcodeApiUrl!, completionHandler: {
+            (data, response, error) in
+            do {
+                let httpResp: NSHTTPURLResponse = response as! NSHTTPURLResponse
+                if(httpResp.statusCode == 200) {
+                    let jsonData = try NSJSONSerialization.JSONObjectWithData(data!, options:NSJSONReadingOptions.MutableContainers ) as! NSDictionary
+                    print(String(jsonData["state"]!))
+                    print(String(jsonData["city"]!))
+                    completionHandler(jsonData)
+                } else {
+                    completionHandler(nil)
+                }
+
+            } catch _ {
+                // Error
+            }
+            
+        });
+        task.resume()
+        return task
+    }
 }
 
